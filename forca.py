5fbe8b35ec7896649848ce231687ee50d0aa48d8
Marcelo Santos <devops@marceleza.com>
2024-02-21 17:03:24 -0300

    updating forca

--- forca.py
+++ forca.py
@@ -8,7 +8,16 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not acertou and not enforcou):
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
         print("Jogando...")
 
     print("Fim do jogo")
